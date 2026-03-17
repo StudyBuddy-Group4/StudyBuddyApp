6558c18fc1c420107244f13406e24066ce2c2d69
AugustDaniel <111312919+AugustDaniel@users.noreply.github.com>
2026-03-17 01:30:17 +0100

    fixed api calls parameters

--- app/src/main/java/com/example/studybuddyapp/api/MatchingApi.java
+++ app/src/main/java/com/example/studybuddyapp/api/MatchingApi.java
@@ -6,13 +6,13 @@ import com.example.studybuddyapp.api.dto.LeaveMeetingResponse;
 
 import retrofit2.Call;
 import retrofit2.http.POST;
-import retrofit2.http.Query;
+import retrofit2.http.Path;
 
 public interface MatchingApi {
 
-    @POST("api/matching/join")
-    Call<JoinMeetingResponse> join(@Query("duration") int duration);
+    @POST("api/matching/join/{duration}")
+    Call<JoinMeetingResponse> join(@Path("duration") int duration);
 
-    @POST("api/matching/leave")
-    Call<LeaveMeetingResponse> leave(@Query("channelName") String channelName);
+    @POST("api/matching/leave/{channelName}")
+    Call<LeaveMeetingResponse> leave(@Path("channelName") String channelName);
 }
