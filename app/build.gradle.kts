4e078fc57dd55f32f021e63eefc3a1b207fc515b
Yingyao-Feng <870197196@qq.com>
2026-03-30 01:11:48 +0200

    Remove Jacoco dependency on Intsrumentes tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,7 +73,7 @@ tasks.register<JacocoReport>("jacocoTestReport") {
 
     // Run BOTH test types before generating report
     dependsOn("testDebugUnitTest")
-    dependsOn("connectedDebugAndroidTest")
+    // dependsOn("connectedDebugAndroidTest") disabled for GitHub actions
     //Configure output formats
     reports {
         xml.required.set(true) // XML -> required for SonarQube
